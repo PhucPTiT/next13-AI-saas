2247aedbaf5b3c30cb95c9af3e82c99ca9d15107
PhucPTiT <phucndah2002@gmail.com>
2023-08-03 15:16:09 +0700

    setup folder and authen

---
+++ ai-saas/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
+const DashBoardPage = () => {
+    return (<div>
+        <UserButton afterSignOutUrl="/"/>
+    </div> );
+}
+ 
+export default DashBoardPage;
